95b48f7ac6253ed0c3a0f685df1f7e634d5984d1
GhaoutiBRH <108005199+GhaoutiBRH@users.noreply.github.com>
2024-02-12 17:53:40 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,61 +1,13 @@
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
-pip install matplotlib
-import matplotlib.pyplot as plt
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-import os
+
                                                                                                                                        
 app = Flask(__name__)
 
-
-
-# Chemin vers le répertoire où se trouve votre script Flask
-directory = os.path.dirname(os.path.realpath(__file__))
-
-# Chemin vers le répertoire 'static'
-static_directory = os.path.join(directory, 'static')
-
-# Création du répertoire 'static' s'il n'existe pas déjà
-if not os.path.exists(static_directory):
-    os.makedirs(static_directory)
-
-
-@app.route('/commits/')
-def show_commit_graph():
-    # Extract commit data from GitHub API
-    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
-    with urllib.request.urlopen(url) as response:
-        data = response.read().decode('utf-8')
-        commits_data = json.loads(data)
-
-    # Process commit data to count commits per minute
-    commits_per_minute = {}
-    for commit in commits_data:
-        commit_date = commit['commit']['author']['date']
-        minute = extract_minutes(commit_date)
-        commits_per_minute[minute] = commits_per_minute.get(minute, 0) + 1
-
-    # Create plot
-    minutes = list(commits_per_minute.keys())
-    commit_counts = list(commits_per_minute.values())
-
-    plt.plot(minutes, commit_counts)
-    plt.xlabel('Minute')
-    plt.ylabel('Number of Commits')
-    plt.title('Commits per Minute')
-    plt.grid(True)
-    plt.savefig('static/commit_graph.png')  # Save plot as a static image file
-    plt.close()
-
-    return render_template('commit_graph.html')
-
-# Helper function to extract minutes from date string
-def extract_minutes(date_string):
-    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-    return date_object.minute
 
 @app.route("/contact/")
 def MaPremiereAPI():
